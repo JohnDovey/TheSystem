619b0eb44050265c195f41a7a7b4616b90bec2f9
John Dovey <dovey.john@gmail.com>
2020-12-31 11:02:27 -0600

    Update Program.cs
    -- Slightly saner Data Access (Break into functions)

--- Program.cs
+++ Program.cs
@@ -24,91 +24,96 @@ namespace TheSystemNet
             Console.WriteLine(Output.BrightBlue("Accessing Database ..."));
 
             // Setup and open database. This is created as a file in the same folder as the program if it doesn't exist
-            using SqliteConnection db = new SqliteConnection("Filename=TheSystem.db");
-            db.Open();
+            SqliteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+            CreateTable(sqlite_conn);
+            InsertData(sqlite_conn);
+            ReadData(sqlite_conn);
 
-            // Create a test table of it doesn't exist
-            String tableCommand = "CREATE TABLE IF NOT EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, Text_Entry NVARCHAR(2048) NULL)";
-            SqliteCommand createTable = new SqliteCommand(tableCommand, db);
-            createTable.ExecuteReader();
+            sqlite_conn.Close(); // Close the database connection. This cleans up all memory etc
 
+        }
+        static SqliteConnection CreateConnection()
+        {
+
+            SqliteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SqliteConnection("Data Source=TheSystem.db; ");
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+        static void InsertData(SqliteConnection conn)
+        {
+
+            // Insert data into the Menu table
+
+            SqliteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+
+
+            sqlite_cmd.CommandText = "INSERT INTO menu (`menuid`, `parentid`, `sortid`, `url`, `name`, `desc`, `seclevel`) VALUES (1, 0, 0, 'index.php', 'Home', 'Return to the website''s home page', 0); ";
+            sqlite_cmd.ExecuteNonQuery();
+           
+            sqlite_cmd.CommandText = "INSERT INTO menu (`menuid`, `parentid`, `sortid`, `url`, `name`, `desc`, `seclevel`) VALUES (2, 0, 1, 'Search.php', 'Search', 'Search for what you need', 0); ";
+            sqlite_cmd.ExecuteNonQuery();
+
+            sqlite_cmd.CommandText = "INSERT INTO menu (`menuid`, `parentid`, `sortid`, `url`, `name`, `desc`, `seclevel`) VALUES (3, 0, 2, '#', 'About This Site', 'All you ever wanted to know about this site', 0); ";
+            sqlite_cmd.ExecuteNonQuery();
+
+            sqlite_cmd.CommandText = "INSERT INTO menu (`menuid`, `parentid`, `sortid`, `url`, `name`, `desc`, `seclevel`) VALUES (4, 3, 1, '#', 'FAQs', 'Frequently Asked Questions (and Frequently Questioned Answers)', 0), (5, 4, 1, 'Advertising_FAQ.php', 'FAQ for advertisers', 'The Important Stuff for advertisers.', 0), (6, 4, 2, 'User_FAQ.php', 'FAQ for users', 'Frequently Asked Questions (and Frequently Questioned Answers) for Users', 0), (7, 3, 3, 'Add_Stuff.php', 'Add Stuff', 'Add Some Stuff', 0), (8, 3, 4, 'Costs.php', 'Costs', 'What part of FREE didn''t you understand?', 0), (9, 8, 2, 'test1.php', 'Test 1', 'This is the first test', 0), (10, 8, 1, 'test2.php', 'Test 2', 'This is the second Test', 0), (11, 0, 10, '#', 'Admin', 'The Admin Area', 10),(12, 11, 1, '#', 'Reports', 'Various reports', 10), (13, 16, 2, '#', 'Edit a Record', 'Admin can edit a record''s content here', 10), (14, 12, 1, 'Admin_listall.php', 'List All ', 'List every little record that there is', 10), (15, 16, 2, 'Admin_Delete.php', 'Delete a Record', 'Be Careful. Once it''s gone, it''s gone!', 1), (16, 11, 2, '#', 'Manage Stuff', '', 10), (17, 11, 5, '#', 'Edit Menu', 'Edit the Actual menu Items', 0), (18, 17, 1, 'Admin_Edit_Menu.php', 'Edit Menu', 'Edit the Actual menu Items', 10), (19, 0, 0, 'https://sourceforge.net/projects/dynamicjqueryme/', 'SourceForge Project', 'Source Forge Project Page', 0), (20, 19, 1, 'https://sourceforge.net/projects/dynamicjqueryme/files/', 'Project Files', 'Project Files', 0), (21, 19, 2, 'https://sourceforge.net/projects/dynamicjqueryme/support', 'Support', 'Support Page', 0); ";
+            sqlite_cmd.ExecuteNonQuery();
+
+        }
+        static void CreateTable(SqliteConnection conn)
+        {
             // Create menu table if it doesn't already exist
-            string menuTableCommand = "CREATE TABLE IF NOT EXISTS menu (menuid INTEGER PRIMARY KEY NOT NULL, parentid INTEGER NOT NULL default(0), sortid INTEGER NOT NULL default(0), url TEXT NOT NULL default('#'), name TEXT NOT NULL default('Unknown'), desc TEXT NOT NULL default('Unknown'), seclevel INTEGER NOT NULL default(0))";
-            SqliteCommand createMenuTable = new SqliteCommand(menuTableCommand, db);
-            createMenuTable.ExecuteReader();
+            SqliteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE IF NOT EXISTS menu (menuid INTEGER PRIMARY KEY NOT NULL, parentid INTEGER NOT NULL default(0), sortid INTEGER NOT NULL default(0), url TEXT NOT NULL default('#'), name TEXT NOT NULL default('Unknown'), desc TEXT NOT NULL default('Unknown'), seclevel INTEGER NOT NULL default(0))";
+            // string Createsql1 = "CREATE TABLE SampleTable1 (Col1 VARCHAR(20), Col2 INT)";
+            sqlite_cmd = conn.CreateCommand();
 
-            // Insert data into the test table
-            SqliteCommand insertCommand = new SqliteCommand();
-            insertCommand.Connection = db;
-
-            insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, 'Here is a new entry in the Database...');";
-
-            insertCommand.ExecuteReader();
+            sqlite_cmd.CommandText = "Drop Table menu;";
+            sqlite_cmd.ExecuteNonQuery();
 
 
-            // Select text from the test table
-            SqliteCommand selectCommand = new SqliteCommand("SELECT Text_Entry from MyTable", db);
-            SqliteDataReader query;
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+            // sqlite_cmd.CommandText = Createsql1;
+            // sqlite_cmd.ExecuteNonQuery();
 
-            query = selectCommand.ExecuteReader();
+        }
+        static void ReadData(SqliteConnection conn)
+        {
 
-            while (query.Read()) // Loop through the test tanle and write out text from the first field
+            // Select text from the Menu table
+
+           // while (query.Read()) // Loop through the menu data and write out text from the fields
+           // {
+           //     Console.WriteLine(query.GetString(0) + " = " + query.GetString(1) + " = " + query.GetString(2) + " = " + query.GetString(3) + " = " + query.GetString(4) + " = " + query.GetString(5));// First selected field is index 0
+            
+
+            SqliteDataReader sqlite_datareader;
+            SqliteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM menu";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
             {
-                Console.WriteLine(query.GetString(0));// First selected field is index 0
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(sqlite_datareader.GetString(0) + " = " + sqlite_datareader.GetString(1) + " = " + sqlite_datareader.GetString(2) + " = " + sqlite_datareader.GetString(3) + " = " + sqlite_datareader.GetString(4) + " = " + sqlite_datareader.GetString(5));// First selected field is index 0
+
+               // Console.WriteLine(myreader);
             }
 
-            db.Close(); // Close the database connection. This cleans up all memory etc
-
-
-            // Ascii Characters
-
-            int topleft = 218;
-            int hline = 196;
-            int topright = 191;
-            int vline = 179;
-            int bottomleft = 192;
-            int bottomright = 217;
-            Console.OutputEncoding = System.Text.Encoding.GetEncoding(28591);
-
-            //draw top left corner
-            Write(topleft);
-            //draw top horizontal line
-            for (int i = 0; i < 10; i++)
-                Write(hline);
-            //draw top right corner
-            Write(topright);
-            Console.WriteLine();
-            //draw left and right vertical lines
-            for (int i = 0; i < 6; i++)
-            {
-                Write(vline);
-                for (int k = 0; k < 10; k++)
-                {
-                    Console.Write(" ");
-                }
-                WriteLine(vline);
-            }
-            //draw bottom left coner
-            Write(bottomleft);
-            //draw bottom horizontal line
-            for (int i = 0; i < 10; i++)
-                Write(hline);
-            //draw bottom right coner
-            Write(bottomright);
-            Console.ReadKey();
         }
-        static void Write(int charcode)
-        {
-            Console.Write((char)charcode);
-        }
-        static void WriteLine(int charcode)
-        {
-            Console.WriteLine((char)charcode);
-        }
-
-
-
-        // End AScii
-
     }
 }
