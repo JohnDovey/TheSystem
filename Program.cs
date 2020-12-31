a70dc18c92fd3685c969739a43a7e1861672e182
John Dovey <dovey.john@gmail.com>
2020-12-30 21:46:35 -0500

    Update Program.cs
    — Added creation of the menu table
    — Added comments to explain the code

--- Program.cs
+++ Program.cs
@@ -23,32 +23,41 @@ namespace TheSystemNet
 
             Console.WriteLine(Output.BrightBlue("Accessing Database ..."));
 
-
+// Setup and open database. This is created as a file in the same folder as the program if it doesn't exist
             using SqliteConnection db = new SqliteConnection("Filename=TheSystem.db");
             db.Open();
 
+            // Create a test table of it doesn't exist
             String tableCommand = "CREATE TABLE IF NOT EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, Text_Entry NVARCHAR(2048) NULL)";
             SqliteCommand createTable = new SqliteCommand(tableCommand, db);
             createTable.ExecuteReader();
-
+            
+            // Create menu table if it doesn't alrrady exist
+            string menuTableCommand = "CREATE TABLE IF NOT EXISTS menu (menuid INTEGER PRIMARY KEY NOT NULL, parentid INTEGER NOT NULL default(0), sortid INTEGER NOT NULL default(0), url TEXT NOT NULL default('#'), name TEXT NOT NULL default('Unknown'), desc TEXT NOT NULL default('Unknown'), seclevel INTEGER NOT NULL default(0))";
+            SqliteCommand createTable =  new SqliteCommand(menuTableCommand, db);
+            createTable.ExecuteReader();
+            
+// Insert data into the test table
             SqliteCommand insertCommand = new SqliteCommand();
             insertCommand.Connection = db;
 
             insertCommand.CommandText = "INSERT INTO MyTable VALUES (NULL, 'Here is a new entry in the Database...');";
 
             insertCommand.ExecuteReader();
-
+            
+            
+// Select text from the test table
             SqliteCommand selectCommand = new SqliteCommand("SELECT Text_Entry from MyTable", db);
             SqliteDataReader query;
 
             query = selectCommand.ExecuteReader();
 
-            while (query.Read())
+            while (query.Read()) // Loop through the test tanle and write out text from the first field
             {
-                Console.WriteLine(query.GetString(0));
+                Console.WriteLine(query.GetString(0));// First selected field is index 0
             }
 
-            db.Close();
+            db.Close(); // Close the database connection. This cleans up all memory etc
 
 
             // Ascii Characters
